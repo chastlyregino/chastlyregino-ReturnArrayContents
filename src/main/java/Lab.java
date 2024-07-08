2418b7659e9fe65a464875af35f0c2995133c575
Finn Chastly Manapat <chastly_regino@icloud.com>
2024-07-08 17:47:00 +0000

    turning array value into spaced-separated string

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -32,6 +32,12 @@ public class Lab {
      * @return a string representation of all values of nums, with the values separated by spaces.
      */
     public String arrayToString(int[] nums){
-        return "";
+        String arrayIntoString = "";
+
+        for (Object number : nums) {
+            arrayIntoString = arrayIntoString + number + " ";
+        }
+
+        return arrayIntoString;
     }
 }
